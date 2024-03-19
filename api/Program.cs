57b4254562b3eeaf76fa326117ac74cb0f79a7ca
John Evil <45630971+joaoivo@users.noreply.github.com>
2024-03-19 18:31:10 -0300

    adicionano CORS

--- api/Program.cs
+++ api/Program.cs
@@ -6,7 +6,23 @@ using System.Text.Json.Nodes;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:Default"]);
+// Adicione o CORS no ConfigureServices
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
+
 var app = builder.Build();
+
+// Configure o CORS no Configure
+app.UseCors();
 
 IResult returnResults(EN_Return result_en, string operation=""){
 	
